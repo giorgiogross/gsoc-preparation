7310228a79817203e39c57a463c17acb6dae4c39
Giorgio Groß <uvech@student.kit.edu>
2017-03-23 22:28:22 +0100

    Add serial port spammer .ino file for Arduino

---
+++ io_buffer_userspace/arduino/uart_spammer/uart_spammer.ino
@@ -0,0 +1,27 @@
+/**
+Use this code to spam the NUM_MSGS messages via the TX1 pin of the Arduino,
+each of them having 1 byte and thus representing one package sent via UART.
+After waiting PAUSE_TIME milliseconds the procedure is repeated.
+
+This sketch helps in testing the serial communication on the BeagleBone Black.
+*/
+#define NUM_MSGS 200
+#define PAUSE_TIME 10000
+#define CONROL_LED 13
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  
+  pinMode(CONROL_LED, OUTPUT);
+}
+
+void loop() {
+  // signal start of transmission
+  digitalWrite(CONROL_LED, HIGH);
+  delay(PAUSE_TIME);
+  digitalWrite(CONROL_LED, LOW);
+  for(int i = 0; i < NUM_MSGS; i++){
+    Serial1.write('1');
+  }
+}
