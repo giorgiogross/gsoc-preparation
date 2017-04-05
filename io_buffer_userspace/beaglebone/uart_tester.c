a673117ae02af9d3de7df78f449c64cd349dc3ff
Giorgio Gross <contact@giorgio-gross.de>
2017-04-06 01:39:13 +0200

    Add license

--- io_buffer_userspace/beaglebone/uart_tester.c
+++ io_buffer_userspace/beaglebone/uart_tester.c
@@ -1,4 +1,22 @@
 /*
+    gsoc-preparation  Serial port communication and web servers on BeagleBone Black
+    Copyright (C) 2017  Giorgio Gross   <contact@giorgio-gross.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
 This program will read input data received on logfile_UART1
 UART2, UART 4 and UART5 on the BeagleBone and count how many 
 packages are received. The messages are then written to four
