bfe8cd86ccdc8d9fa35c73fad82ea57128f5ca27
Giorgio Gross <contact@giorgio-gross.de>
2017-04-06 01:26:47 +0200

    Add license

--- io_buffer_userspace/arduino/uart_spammer_long_msgs/uart_spammer_long_msgs.ino
+++ io_buffer_userspace/arduino/uart_spammer_long_msgs/uart_spammer_long_msgs.ino
@@ -6,6 +6,24 @@ After waiting PAUSE_TIME milliseconds the procedure is repeated.
 This sketch helps in testing the serial communication on the BeagleBone Black.
 */
 
+/*
+    gsoc-preparation  Serial port communication and web servers on BeagleBone Black
+    Copyright (C) 2017  Giorgio Gross
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #define NUM_MSGS 1
 #define PAUSE_TIME 3000
 #define CONROL_LED 13
