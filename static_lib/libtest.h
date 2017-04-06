83ca3aac5a2009c71b96ba26a39dff9a4d3b0805
Giorgio Gross <contact@giorgio-gross.de>
2017-04-06 16:05:44 +0200

    Add example to create static libraries

---
+++ static_lib/libtest.h
@@ -0,0 +1,24 @@
+/*
+    gsoc-preparation  Serial port communication and web servers on BeagleBone Black
+    Copyright (C) 2017  Giorgio Gross   <contact@giorgio-gross.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+
+/*
+Simple static library with a single function to get experience in creating libraies.
+*/
+void printHello(void);
---
+++ static_lib/main.c
@@ -0,0 +1,7 @@
+
+#include "libtest.h"
+
+int main() {
+    printHello();
+    return 0;
+}
